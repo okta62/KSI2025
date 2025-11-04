b6840507239fccef74de61760735c6da7ef3fe84
okta62 <oktalestari2004@gmail.com>
2025-11-04 14:33:11 +0700

    Menambahkan fitur koneksi ke database

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // Program utama
+console.log("Aplikasi siap dijalankan...");
+
+// Fitur koneksi ke database
+function connectDatabase() {
+    const mysql = require('mysql');
+    const connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "db_mahasiswa"
+    });
+
+    connection.connect(function(err) {
+        if (err) throw err;
+        console.log("✅ Berhasil terkoneksi ke database!");
+    });
+}
+
+connectDatabase();
+
